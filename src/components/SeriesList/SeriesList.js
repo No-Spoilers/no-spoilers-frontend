eb6739b14a90aba11d86a4d0a32dd9ae63c89cb1
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2021-03-17 14:34:44 -0700

    convert to function

--- src/components/SeriesList/SeriesList.js
+++ src/components/SeriesList/SeriesList.js
@@ -1,67 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './SeriesList.css';
 
-class SeriesList extends Component {
-  state = {
-    isFetching: false,
-    series: [],
-    books: [],
-    entries: [],
-    selectedSeries: null
+const SeriesList = (props) => {
+  let loadingDiv = <div></div>;
+  if (props.isFetching) {
+    loadingDiv = <div>LOADING DATA</div>
   }
 
-  componentDidMount()  {
-    this.getSeriesList();
-  }
-
-  getSeriesList = async () => {
-    this.setState({isFetching: true});
-
-    const response = await fetch('https://api.no-spoilers.net/series');
-    const body = await response.json();
-
-    this.setState({
-      isFetching: false,
-      series: body
-    });
-  }
-  
-  render() {
-    let loading = <div></div>;
-    if (this.state.isFetching) {
-      loading = <div>LOADING DATA</div>
-    }
-
-    let seriesList = <div></div>;
-    if (this.state.series.length > 0) {
-      seriesList = (
-        <div className="series-list">
-          <div className="series-list-header-container">
-            <div className="series-list-header-title">Series List</div>
-            <Link to='/new'><div className="series-add-button">Add New<br/>Series</div></Link>
+  let seriesListDiv = <div></div>;
+  if (props.seriesList.length > 0) {
+    seriesListDiv = (
+      <div className="series-list">
+        <div className="series-list-header-container">
+          <div className="series-list-header-title">Series List</div>
+          <Link to='/new'><div className="series-add-button">Add New<br/>Series</div></Link>
+        </div>
+        {props.seriesList.map(item => (
+          <div key={item.seriesId} className="series-name">
+            <Link to={`/${item.seriesId}`}><div>{item.name}</div></Link>
           </div>
-          {this.state.series.map(item => (
-            <div key={item.seriesId} className="series-name">
-              <Link to={`/${item.seriesId}`}><div>{item.name}</div></Link>
-            </div>
-          ))}
-        </div>
-      );
-    }
-
-
-    return (
-      <div className="series-list-container">
-        
-        <div>
-          {loading}
-          {seriesList}
-        </div>
-
+        ))}
       </div>
     );
   }
+
+  return (
+    <div className="series-list-container">
+      
+      <div>
+        {loadingDiv}
+        {seriesListDiv}
+      </div>
+
+    </div>
+  );
 }
 
 export default SeriesList;
