4dcc3b8508ed72d109b7b73e43e82069edd5adf3
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2021-05-19 11:53:57 -0700

    save changes to spoiler level through dropdown

--- src/components/EntryView/EntryView.js
+++ src/components/EntryView/EntryView.js
@@ -23,6 +23,16 @@ const mostRecentEntry = (text, spoilId, orderedBookList) => {
   return null;
 }
 
+const earliestMentionId = (text = {}, orderedBookList, index = 0) => {
+  if (text[orderedBookList[index]]) return orderedBookList[index];
+
+  if (orderedBookList.length > index + 1) {
+    return earliestMentionId(text, orderedBookList, index + 1)
+  }
+
+  return null;
+}
+
 const EntryView = (props) => {
   const entryId = useParams().contentId;
   const seriesId = props.lookUp[entryId];
@@ -37,23 +47,40 @@ const EntryView = (props) => {
     }
   }, [entry, getSeriesDetail, entryId]);
 
+  const spoilerChangeHandler = (event) => {
+    props.setSpoilerLevel(seriesId, event.target.value);
+  }
 
   const seriesName = seriesDetails?.[seriesId]?.name || '';
   const breadcrumb = seriesName ? `>> ${seriesName}` : ''; // TODO: Expand to include book when linked from bookview
 
-  const spolierLevelBookId = props.userSpoilerLevel[seriesId];
-  const bookList = seriesDetails?.[seriesId]?.books;
-  const spoilerLevelBookName = bookList?.[spolierLevelBookId]?.name || '-- no level set --';
+  const spolierLevelBookId = props.userSpoilerLevel[seriesId] || '';
+  const bookList = seriesDetails?.[seriesId]?.books || {};
 
-  let spoilerLevelText = (
-    <div>
-      <p>This article potentially contains spoilers through <span className="spoiler-level-book">{spoilerLevelBookName}.</span></p>
-    </div>
+  const spoilerChoices = Object.values(bookList).sort(sortByTimestamp(-1)).map(book => (
+    <option value={book.bookId} key={book.bookId}>{book.name}</option>
+  ));
+
+  spoilerChoices.unshift(<option value="" key="1">-- No Spoilers --</option>);
+
+  const spoilerLevelDropdown = (
+    <select name="books" id="books" className="spoiler-level-book" value={spolierLevelBookId || "0"} onChange={spoilerChangeHandler}>
+      {spoilerChoices}
+    </select>
   )
 
-  const entryText = mostRecentEntry(entry.text, spolierLevelBookId, bookOrder(bookList));
+  let spoilerLevelText = <label htmlFor="spolierLevelBook">Set spoiler level: {spoilerLevelDropdown}</label>
+  let entryViewHtml;
 
-  const entryViewHtml = entryText ? entryText : <p>{entry.name} is mentioned in {seriesName}.</p>;
+  if (spolierLevelBookId !== '') {
+    spoilerLevelText = <label htmlFor="spolierLevelBook">This article potentially contains spoilers through {spoilerLevelDropdown}</label>
+    
+    const entryText = mostRecentEntry(entry.text, spolierLevelBookId, bookOrder(bookList));
+    entryViewHtml = entryText ? entryText : <p>{entry.name} is mentioned in {seriesName}.</p>;
+  } else {
+    const firstMention = bookList[earliestMentionId(entry.text, bookOrder(bookList))]?.name;
+    entryViewHtml = firstMention ? <p>{entry.name} is first mentioned in {firstMention}.</p> : <p>{entry.name} has no entries yet.</p>;
+  }
 
   return (
     <div className="entry-view-container">
@@ -61,7 +88,9 @@ const EntryView = (props) => {
         <div className='breadcrumb-text'>{breadcrumb}</div>
       </Link>
       <div className="spoiler-level-warning">
-        {spoilerLevelText}
+        <form>
+          {spoilerLevelText}
+        </form>
       </div>
 
       <div className="entry-html">
